7dc72e59a9d4621695d7a32dd53ba5cf03651883
luswdev <610721230@gms.ndhu.edu.tw>
2022-02-20 00:19:42 +0800

    reduce splatnet api code

--- src/splatnet.php
+++ src/splatnet.php
@@ -1,97 +1,62 @@
 <?php
 
-include_once __DIR__.'/splatnet-config.php';
+include_once __DIR__ . '/splatnet-config.php';
 
 header('Content-Type: text/plain; charset=utf-8');
 
 /*********************** build config header ***********************/
 
-$app_timezone_offset;
-$app_unique_id;
-$app_user_agent;
-$api_key = config['api_key'];
-$api_cookie = config['api_cookie'];
-$session_token = config['session_token'];
-$user_lang = config['user_lang'];
+$api_key             = SPLATNET_CONFIG['api_key'];
+$api_cookie          = SPLATNET_CONFIG['api_cookie'];
+$session_token       = SPLATNET_CONFIG['session_token'];
+$user_lang           = SPLATNET_CONFIG['user_lang'];
+$app_unique_id       = SPLATNET_CONFIG['app_unique_id'] ?? '32449507786579989234';
+$app_user_agent      = SPLATNET_CONFIG['app_user_agent'] ?? 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36';
+$app_timezone_offset = SPLATNET_CONFIG['app_timezone_offset'] ?? get_timezone_offset();
 
-if(array_key_exists('app_timezone_offset', config)) {
-    $app_timezone_offset = config['app_timezone_offset'];
-} else {
-    $ori_time_zone = new DateTimeZone("UTC");
-    $cur_time_zone = new DateTimeZone(date_default_timezone_get());
-
-    $ori_date = new DateTime("now", $ori_time_zone);
-    $cur_date = new DateTime("now", $cur_time_zone);
-
-    $time_ofst = $ori_time_zone->getOffset($cur_date);
-
-    if ($time_ofst == 0) {
-        $time_ofst = 0 - $cur_time_zone->getOffset($ori_date);
-    }
-
-    $app_timezone_offset = $time_ofst / 60;
-}
-
-if(array_key_exists('app_unique_id', config)) {
-    $app_unique_id = config['app_unique_id'];
-} else {
-    $app_unique_id = '32449507786579989234';
-}
-
-if(array_key_exists('app_user_agent', config)) {
-    $app_user_agent = config['app_user_agent'];
-} else {
-    $app_user_agent = 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36';
-}
-
-$curl_opt = array(
-    'method' => 'get',
-    'url' => 'https://app.splatoon2.nintendo.net/api/',
-    'headers' => array(
-        'Host' => 'app.splatoon2.nintendo.net',
-        'x-unique-id' => $app_unique_id,
-        'x-requested-with' => 'XMLHttpRequest',
-        'x-timezone-offset' => strval($app_timezone_offset),
-        'User-Agent' => $app_user_agent,
-        'Accept' => '*/*',
-        'Referer' => 'https://app.splatoon2.nintendo.net/home',
-        'Accept-Encoding' => 'gzip, deflate',
-        'Accept-Language' => $user_lang,
-    ),
-    'Cookie' => array(
-        'iksm_session' => $api_cookie
-    )
-);
+const API_BASE_URL = 'https://app.splatoon2.nintendo.net/api/';
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_opt['headers']);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-curl_setopt($ch, CURLOPT_COOKIE, 'iksm_session='.$curl_opt['Cookie']['iksm_session']);
+curl_setopt_array($ch, array (
+    CURLOPT_HEADER          =>  0,
+    CURLOPT_RETURNTRANSFER  =>  true,
+    CURLOPT_COOKIE          => 'iksm_session=' . $api_cookie,
+    CURLOPT_HTTPHEADER      => array (
+        'Host'              => 'app.splatoon2.nintendo.net',
+        'x-unique-id'       => $app_unique_id,
+        'x-requested-with'  => 'XMLHttpRequest',
+        'x-timezone-offset' => strval($app_timezone_offset),
+        'User-Agent'        => $app_user_agent,
+        'Accept'            => '*/*',
+        'Referer'           => 'https://app.splatoon2.nintendo.net/home',
+        'Accept-Encoding'   => 'gzip, deflate',
+        'Accept-Language'   => $user_lang,
+    )
+));
 
 /*********************** api: records ***********************/
 
-curl_setopt($ch, CURLOPT_URL, $curl_opt['url'].'records');
+curl_setopt($ch, CURLOPT_URL, API_BASE_URL . 'records');
 $ret_records = json_decode(curl_exec($ch), true);
 write_json($ret_records, '/../log/splatnet-records.json');
 
 /*********************** api: results ***********************/
 
-curl_setopt($ch, CURLOPT_URL, $curl_opt['url'].'results');
+curl_setopt($ch, CURLOPT_URL,  API_BASE_URL . 'results');
 $ret_results = json_decode(curl_exec($ch), true);
 write_json($ret_results, '/../log/splatnet-results.json');
 
 /*********************** api: nickname_and_icon ***********************/
 
 $id = $ret_records['records']['player']['principal_id'];
-curl_setopt($ch, CURLOPT_URL, $curl_opt['url'].'nickname_and_icon?id='.$id);
+curl_setopt($ch, CURLOPT_URL,  API_BASE_URL . 'nickname_and_icon?id='.$id);
 $ret_nick = json_decode(curl_exec($ch), true);
 write_json($ret_nick, '/../log/splatnet-nick.json');
 
 /*********************** api: records/hero ***********************/
 
-curl_setopt($ch, CURLOPT_URL, $curl_opt['url'].'records/hero');
+curl_setopt($ch, CURLOPT_URL,  API_BASE_URL . 'records/hero');
 $ret_hero = json_decode(curl_exec($ch), true);
 write_json($ret_hero, '/../log/splatnet-hero.json');
 curl_close($ch);
@@ -173,9 +138,27 @@ write_json($splatnet, '/../splatnet-data.json');
 
 function write_json(array $json, string $file_path)
 {
-    $output_file = __DIR__.$file_path;
+    $output_file = __DIR__ . $file_path;
     touch($output_file);
     $fp = fopen($output_file, 'w');
     fwrite($fp, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     fclose($fp);
+}
+
+function get_timezone_offset() : int
+{
+    $ori_time_zone = new DateTimeZone("UTC");
+    $cur_time_zone = new DateTimeZone(date_default_timezone_get());
+
+    $ori_date = new DateTime("now", $ori_time_zone);
+    $cur_date = new DateTime("now", $cur_time_zone);
+
+    $time_ofst = $ori_time_zone->getOffset($cur_date);
+
+    if ($time_ofst == 0) {
+        $time_ofst = 0 - $cur_time_zone->getOffset($ori_date);
+    }
+
+    $app_timezone_offset = $time_ofst / 60;
+    return $app_timezone_offset;
 }
