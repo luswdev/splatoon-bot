f95a71901154c8c474ccd36b3bb153c344eac841
luswdev <610721230@gms.ndhu.edu.tw>
2022-09-20 20:26:39 +0800

    modified. coding style

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,22 +1,22 @@
-function set_cookie(cname, cvalue, exdays, cpath) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/" + cpath;
+function set_cookie(_cname, _cvalue, _exdays, _cpath) {
+    const d = new Date()
+    d.setTime(d.getTime() + (_exdays * 24 * 60 * 60 * 1000))
+    let expires = `expires=${d.toUTCString()}`
+    document.cookie = `${_cname}=${_cvalue};${expires};path=/${_cpath}`
 }
 
-function get_cookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
+function get_cookie(_cname) {
+    let name = `${_cname}=`
+    let decoded_cookie = decodeURIComponent(document.cookie)
+    let ca = decoded_cookie.split(';')
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i]
         while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length)
         }
     }
-    return "";
+    return ''
 }
