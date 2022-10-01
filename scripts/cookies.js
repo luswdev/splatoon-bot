4ca68448ea9e397aa40fdd45864e7c6723cf6e65
luswdev <610721230@gms.ndhu.edu.tw>
2022-10-01 23:13:06 +0800

    modified. user friendly

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -6,23 +6,23 @@
 
 class Cookies {
     set(_cname, _cvalue, _exdays, _cpath) {
-        const d = new Date()
-        d.setTime(d.getTime() + (_exdays * 24 * 60 * 60 * 1000))
-        let expires = `expires=${d.toUTCString()}`
+        const now = new Date()
+        now.setTime(now.getTime() + (_exdays * 24 * 60 * 60 * 1000))
+        let expires = `expires=${now.toUTCString()}`
         document.cookie = `${_cname}=${_cvalue};${expires};path=/${_cpath}`
     }
 
     get(_cname) {
         let name = `${_cname}=`
         let decoded_cookie = decodeURIComponent(document.cookie)
-        let ca = decoded_cookie.split(';')
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i]
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1)
+        let cookies_arr = decoded_cookie.split(';')
+        for (let i = 0; i < cookies_arr.length; i++) {
+            let cookie = cookies_arr[i]
+            while (cookie.charAt(0) == ' ') {
+                cookie = cookie.substring(1)
             }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length)
+            if (cookie.indexOf(name) == 0) {
+                return cookie.substring(name.length, cookie.length)
             }
         }
         return ''
