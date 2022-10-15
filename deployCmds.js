190246004e505d6524c46302bf2a34db8b6f164b
luswdev <610721230@gms.ndhu.edu.tw>
2022-10-16 04:14:12 +0800

    add. start script from package.json

--- deployCmds.js
+++ deployCmds.js
@@ -11,5 +11,5 @@ const commands = getCmdsJson()
 const rest = new REST({ version: '10' }).setToken(token)
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
-    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .then((data) => console.log(`[${__filename}] Successfully registered ${data.length} application commands.`))
     .catch(console.error)
